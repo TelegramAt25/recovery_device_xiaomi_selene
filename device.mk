7a5fc0ccdec749da8a1e435a9edb2c89b71184ac
fukiame <fukiame@proton.me>
2023-06-01 08:13:31 +0700

    selene: revert keymaster update
    an a12 tree will be ~~sold~~ made separatedly
    
    Revert "selene: fixup vintf for updated keymaster"
    Revert "selene: fixup keymaster update"
    Revert "selene: partially revert "selene: fixup""
    Revert "selene: update keymaster blob from S"

--- device.mk
+++ device.mk
@@ -54,12 +54,10 @@ PRODUCT_PACKAGES += \
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
-    libkeymaster41 \
     libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Health HAL
