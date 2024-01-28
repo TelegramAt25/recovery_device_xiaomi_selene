ad54576574b4518650156393848ed7c8a75c7812
TillYourTearGoes <tillyourteargoes@proton.me>
2024-01-28 16:45:36 +0700

    selene: kang vibrator aidl from rosemary
    original code from https://github.com/RedmiNote10S/recovery_device_redmi_rosemary
    
    Signed-off-by: TillYourTearGoes <tillyourteargoes@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,7 @@ TW_CUSTOM_BATTERY_POS := "790"
 # Device
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 600
-TW_NO_HAPTICS := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
