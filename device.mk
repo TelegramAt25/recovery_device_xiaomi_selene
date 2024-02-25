e413f2375ef28ff67f1ecdcf6bda0cab662d4017
Hiura Mihate <tillyourteargoes@murena.io>
2024-02-25 07:57:38 +0700

    Revert "selene: bootctrl: Update to version 1.2"
    This reverts commit 072dfc2f1286f3bc0578357a22b56399ba364d02.

--- device.mk
+++ device.mk
@@ -19,12 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
+    bootctrl.mt6768 \
     update_engine_client
 
 PRODUCT_PACKAGES += \
